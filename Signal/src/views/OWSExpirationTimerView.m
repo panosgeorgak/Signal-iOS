d7409734d88ffb986feacaabfb86545b88ae7b41
Michael Kirk <michael.code@endoftheworl.de>
2016-09-30 14:56:41 -0400

    animation timer per message

--- Signal/src/views/OWSExpirationTimerView.m
+++ Signal/src/views/OWSExpirationTimerView.m
@@ -2,6 +2,7 @@
 //  Copyright © 2016 Open Whisper Systems. All rights reserved.
 
 #import "OWSExpirationTimerView.h"
+#import <QuartzCore/CAShapeLayer.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
@@ -11,6 +12,10 @@ NS_ASSUME_NONNULL_BEGIN
 @property (nonatomic) NSTimer *animationTimer;
 @property (atomic) uint64_t expiresAtSeconds;
 
+@property (nonatomic, readonly) UIImageView *emptyHourglassImageView;
+@property (nonatomic, readonly) UIImageView *fullHourglassImageView;
+@property CGFloat ratioRemaining;
+
 @end
 
 @implementation OWSExpirationTimerView
@@ -19,6 +24,66 @@ NS_ASSUME_NONNULL_BEGIN
 {
     DDLogDebug(@"%@ invalidating old animation timer in dealloc", self.logTag);
     [_animationTimer invalidate];
+}
+
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (!self) {
+        return self;
+    }
+
+    _emptyHourglassImageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"ic_hourglass_empty"]];
+    [self insertSubview:_emptyHourglassImageView atIndex:0];
+
+    _fullHourglassImageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"ic_hourglass_full"]];
+    [self insertSubview:_fullHourglassImageView atIndex:1];
+    _fullHourglassImageView.clipsToBounds = YES;
+
+    _ratioRemaining = 1.0f;
+    return self;
+}
+
+- (nullable instancetype)initWithCoder:(NSCoder *)aDecoder
+{
+    self = [super initWithCoder:aDecoder];
+    if (!self) {
+        return self;
+    }
+
+    _emptyHourglassImageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"ic_hourglass_empty"]];
+    [self insertSubview:_emptyHourglassImageView atIndex:0];
+
+    _fullHourglassImageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"ic_hourglass_full"]];
+    [self insertSubview:_fullHourglassImageView atIndex:1];
+    _fullHourglassImageView.clipsToBounds = YES;
+
+    _ratioRemaining = 1.0f;
+
+    return self;
+}
+
+- (void)layoutSubviews
+{
+    CGFloat leftMargin = 6.0f;
+    CGRect hourglassFrame = CGRectMake(leftMargin, 0, self.frame.size.height, self.frame.size.height);
+    self.emptyHourglassImageView.frame = hourglassFrame;
+    self.fullHourglassImageView.frame = hourglassFrame;
+
+    CGFloat yOffset = (1 - self.ratioRemaining) * hourglassFrame.size.height;
+
+    // Lifted from http://stackoverflow.com/questions/11391058/simply-mask-a-uiview-with-a-rectangle
+    // Create a mask layer and the frame to determine what will be visible in the view.
+    CAShapeLayer *maskLayer = [[CAShapeLayer alloc] init];
+    CGRect maskRect = CGRectOffset(hourglassFrame, -leftMargin, yOffset);
+    // Create a path with the rectangle in it.
+    CGPathRef path = CGPathCreateWithRect(maskRect, NULL);
+    // Set the path to the mask layer.
+    maskLayer.path = path;
+    // Release the path since it's not covered by ARC.
+    CGPathRelease(path);
+
+    self.fullHourglassImageView.layer.mask = maskLayer;
 }
 
 - (void)endAnyTimer
@@ -44,16 +109,15 @@ NS_ASSUME_NONNULL_BEGIN
         self.logTag,
         expiresAtSeconds,
         initialDurationSeconds);
+
     self.expiresAtSeconds = expiresAtSeconds;
     self.initialDurationSeconds = initialDurationSeconds;
-
 
     [self animateTimer];
 
     __weak typeof(self) wself = self;
-
     self.animationTimer =
-        [NSTimer scheduledTimerWithTimeInterval:initialDurationSeconds / 10.0 // assumes a 10 step timer animation.
+        [NSTimer scheduledTimerWithTimeInterval:initialDurationSeconds / 16.0 // assumes a 10 step timer animation.
                                          target:wself
                                        selector:@selector(animateTimer)
                                        userInfo:nil
@@ -66,11 +130,15 @@ NS_ASSUME_NONNULL_BEGIN
 
     if (secondsLeft > INT_MAX) { // overflow
         secondsLeft = 0;
+        DDLogDebug(@"%@ Ending timer because time ran out.", self.logTag);
+        [self endAnyTimer];
     }
 
     DDLogVerbose(@"%@ Animating timer with seconds left: %f", self.logTag, secondsLeft);
-    // TODO better animation.
-    self.alpha = ((CGFloat)secondsLeft / (CGFloat)self.initialDurationSeconds);
+    self.ratioRemaining = (CGFloat)secondsLeft / (CGFloat)self.initialDurationSeconds;
+
+    [self setNeedsLayout];
+    [self layoutIfNeeded];
 }
 
 #pragma mark - Logging
